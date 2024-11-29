c16d06b8b1bf75b4619141c4c1add302d07cfc0f
Username1256-svg <mingazutdinovan@gmail.com>
2024-11-29 14:48:14 +0500

    Update webview_page.dart

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatefulWidget {
-  const WebViewPage({super.key});
+  const WebViewPage({Key? key}) : super(key: key);
 
   @override
   State<WebViewPage> createState() => _WebViewPageState();
@@ -14,19 +14,47 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   void initState() {
     super.initState();
-
-    // Настройка контроллера WebView
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'Flutter',
+        onMessageReceived: (message) {
+          _handleJSMessage(message.message);
+        },
+      )
       ..loadFlutterAsset('assets/js/index.html');
+  }
+
+  void _handleJSMessage(String message) {
+    if (message == 'showAd') {
+      _showAd(); // Реализация показа рекламы
+    } else if (message == 'shareApp') {
+      _shareApp();
+    } else if (message.startsWith('setOrientation:')) {
+      _setOrientation(message.split(':')[1]);
+    }
+  }
+
+  void _showAd() {
+    // Реализация показа рекламы
+  }
+
+  void _shareApp() {
+    // Шеринг через Flutter package (например, `share_plus`)
+  }
+
+  void _setOrientation(String orientation) {
+    if (orientation == 'portrait') {
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    } else if (orientation == 'landscape') {
+      SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Index.html Viewer'),
-      ),
+      appBar: AppBar(title: const Text('WebView Page')),
       body: WebViewWidget(controller: _controller),
     );
   }
