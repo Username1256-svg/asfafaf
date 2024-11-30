159863eeabf32d5255d9a86fde0b17b150ec8707
HispterOK <stero141@gmail.com>
2024-11-30 15:40:02 +0500

    sfa

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -1,5 +1,6 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewPage extends StatefulWidget {
@@ -16,6 +17,7 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   void initState() {
     super.initState();
+
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel(
@@ -24,13 +26,35 @@ class _WebViewPageState extends State<WebViewPage> {
           _handleJSMessage(message.message);
         },
       )
-      ..loadFlutterAsset('assets/index.html');
+      ..loadRequest(
+        Uri.parse('http://localhost:8080/assets/index.html'), // Используем локальный сервер
+      );
+
+    _startLocalServer();
   }
 
   // Метод для обработки сообщений от JavaScript
   void _handleJSMessage(String message) {
     setState(() {
       _errors = message; // Отображаем ошибки на экране
+    });
+  }
+
+  // Старт локального HTTP-сервера
+  Future<void> _startLocalServer() async {
+
+    final server = await HttpServer.bind(InternetAddress.loopbackIPv4, 8080);
+    server.listen((HttpRequest request) async {
+      final filePath = 'assets/index.html'; // Путь к вашему файлу
+      final file = File(filePath);
+
+      if (await file.exists()) {
+        request.response.headers.contentType = ContentType.html;
+        await request.response.addStream(file.openRead());
+      } else {
+        request.response.statusCode = HttpStatus.notFound;
+      }
+      await request.response.close();
     });
   }
 
